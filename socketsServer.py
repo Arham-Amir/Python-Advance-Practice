218ae440427db703d497a331019e2b017285947a
Arham Amir <arhamamir424@gmail.com>
2024-05-30 13:52:24 +0500

    Socket termination error resolved

--- socketsServer.py
+++ socketsServer.py
@@ -3,6 +3,7 @@ import socket
 
 socketStopEvent = threading.Event()
 
+# Create a socket instance
 socketInstance = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 socketInstance.bind(('127.0.0.1', 5555))
 socketInstance.listen()
@@ -10,20 +11,33 @@ socketInstance.listen()
 def socketListen():
     print("Server listening ...\n")
     while not socketStopEvent.is_set():
-        client, address = socketInstance.accept()
-        print(f'Connected with the client: {address}')
-        client.send("You are Connected Sucessfully".encode())
-        client.close() 
+        try:
+            client, address = socketInstance.accept()
+            print(f'Connected with the client: {address}')
+            client.send("You are Connected Successfully".encode())
+            client.close()
+        except OSError as e:
+            if socketStopEvent.is_set():
+                break
+            else:
+                raise e
 
+# Create and start the socket listening thread
 socketThread = threading.Thread(target=socketListen)
 socketThread.start()
 
+# Wait for user input to terminate the server
+x = input("Enter 't' to terminate the server. ")
+while x.lower() != 't':
+    x = input("Enter 't' to terminate the server. ")
 
-x = input("Enter t to terminate the server. ")
-while(x!='t' and x!='T'):
-    x = input("Enter t to terminate the server. ")
-
+# Set the stop event to terminate the socket listening thread
 socketStopEvent.set()
-socketThread.join()
-print("Server Stopped ...\n")
+
+# Close the socket instance
 socketInstance.close()
+
+# Wait for the socket listening thread to finish
+socketThread.join()
+
+print("Server Stopped ...\n")
