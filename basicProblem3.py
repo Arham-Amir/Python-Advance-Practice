25c6ee32de09dcd1cf14cf1fc8367d8e4a026762
Arham Amir <arhamamir424@gmail.com>
2024-06-03 14:00:41 +0500

    Solution of finding the sum of numbers untill it becomes a single digit

---
+++ basicProblem3.py
@@ -0,0 +1,14 @@
+number = 123456
+
+def sumUptoN(num):
+    if num < 10:
+        return num
+    
+    sum = 0
+    
+    while num > 0:
+        sum += num % 10
+        num //= 10 
+    return sumUptoN(sum)
+
+print(sumUptoN(number))
